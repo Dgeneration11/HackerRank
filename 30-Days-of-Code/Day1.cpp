a8c856c31bb7a58e9c8213eeebd92f275eeeaef2
Bhupendra Gehlot <buntydgeneration@gmail.com>
2021-05-16 23:26:34 +0530

    Day0 - Day3

---
+++ 30-Days-of-Code/Day1.cpp
@@ -0,0 +1,25 @@
+
+
+    int j;
+    double d2;
+    string s1;
+    // Declare second integer, double, and String variables.
+    
+    // Read and save an integer, double, and String to your variables.
+    // Note: If you have trouble reading the entire string, please go back and review the Tutorial closely.
+    
+    // Print the sum of both integer variables on a new line.
+    
+    // Print the sum of the double variables on a new line.
+    
+    // Concatenate and print the String variables on a new line
+    // The 's' variable above should be printed first.
+    cin>>j;
+    cin>>d2;
+    cin.get();
+    getline(cin, s1);
+    
+    
+    cout<<i+j<<endl;
+    cout<<fixed<<setprecision(1)<<d+d2<<endl;
+    cout<<s<<s1<<endl;
