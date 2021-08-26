8807bbcc1a6a157f8f6da222cbbcbf8be1f24ca1
= <buntydgeneration@gmail.com>
2021-08-26 21:19:05 +0530

    new

---
+++ 1 Month Preparation Kit/XOR-strings.cpp
@@ -0,0 +1,25 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+string strings_xor(string s, string t)
+{
+
+    string res = "";
+    for (int i = 0; i < s.size(); i++)
+    {
+        res += to_string(s[i] ^ t[i]);
+    }
+    return res;
+}
+
+int main()
+{
+    string s, t;
+    cin >> s >> t;
+    cout << strings_xor(s, t) << endl;
+    return 0;
+}
